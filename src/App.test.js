64841c62efd79f935c2c8508e3d31190ddeeac63
乔英杰 <3043758468@qq.com>
2019-08-18 23:44:01 +0800

    修改测试

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import reducer from './store/reducers';
+import './styles/base.less';
+import RoutePages from './router';
+
+const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)));
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  ReactDOM.render(
+    <Provider store={store}>
+      <RoutePages />
+    </Provider>,
+    div
+  );
+  ReactDOM.unmountComponentAtNod;
 });
