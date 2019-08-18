f9f0ceb724c4d34030d25ab7f206ee28e5d5eed4
乔英杰 <3043758468@qq.com>
2019-08-18 13:05:03 +0800

    ceshi

--- src/store/reducers/resume.js
+++ src/store/reducers/resume.js
@@ -1,45 +1,61 @@
-import { SAVE_RESUME_LIST, SAVE_RESUME_DIRECT, SAVE_RESUME_CARD_INFO, SAVE_RESUME_TEMPLATES, SAVE_SELECTED_TEMPLATE, SAVE_RESUME_DETAIL, SAVE_DIRECTION, SAVE_SELECT_COURSE, SAVE_EDIT_DATA, SAVE_EDIT_STATUS, SAVE_RESUME_REFERENCE } from "../types";
+import {
+  SAVE_RESUME_LIST,
+  SAVE_RESUME_DIRECT,
+  SAVE_RESUME_CARD_INFO,
+  SAVE_RESUME_TEMPLATES,
+  SAVE_SELECTED_TEMPLATE,
+  SAVE_RESUME_DETAIL,
+  SAVE_DIRECTION,
+  SAVE_SELECT_COURSE,
+  SAVE_EDIT_DATA,
+  SAVE_EDIT_STATUS,
+  SAVE_RESUME_REFERENCE
+} from '../types';
 
 const initState = {
-	onlineResume: [],
-	adjunctResume: {},
-	cardInfo: {},
-	resumeDirection: null,
-	templates: [],
-	selectedTemplate: {},
-	selectedDirection: {},
-	resumeCourse: {},
-	resumeReference: [],
-	resumeDetail: {},
-	editData: {},
-	resumeEditStatus: false
-}
+  onlineResume: [],
+  adjunctResume: {},
+  cardInfo: {},
+  resumeDirection: null,
+  templates: [],
+  selectedTemplate: {},
+  selectedDirection: {},
+  resumeCourse: {},
+  resumeReference: [],
+  resumeDetail: {},
+  editData: {},
+  resumeEditStatus: false
+};
 
 export default function resume(state = initState, action) {
-	switch(action.type) {
-		case SAVE_RESUME_LIST:
-			return { ...state, onlineResume: action.payload.onlineResume, adjunctResume: action.payload.adjunctResume };
-		case SAVE_RESUME_CARD_INFO:
-			return {...state, cardInfo: {...action.payload} };
-		case SAVE_RESUME_DIRECT:
-			return {...state, resumeDirection: action.payload};
-		case SAVE_RESUME_TEMPLATES:
-			return {...state, templates: action.payload};
-		case SAVE_SELECTED_TEMPLATE:
-			return {...state, selectedTemplate: {...action.payload} };
-		case SAVE_RESUME_DETAIL:
-			return {...state, resumeDetail: {...action.payload} };
-		case SAVE_DIRECTION:
-			return { ...state, selectedDirection: {...action.payload} };
-		case SAVE_SELECT_COURSE:
-			return { ...state, resumeCourse: {...action.payload} };
-		case SAVE_EDIT_DATA:
-			return {...state, editData: action.payload};
-		case SAVE_EDIT_STATUS:
-			return { ...state, resumeEditStatus: action.payload};
-		case SAVE_RESUME_REFERENCE:
-			return {...state, resumeReference: action.payload};
-		default:
-			return state;
-	}
+  switch (action.type) {
+    case SAVE_RESUME_LIST:
+      return {
+        ...state,
+        onlineResume: action.payload.onlineResume,
+        adjunctResume: action.payload.adjunctResume
+      };
+    case SAVE_RESUME_CARD_INFO:
+      return { ...state, cardInfo: { ...action.payload } };
+    case SAVE_RESUME_DIRECT:
+      return { ...state, resumeDirection: action.payload };
+    case SAVE_RESUME_TEMPLATES:
+      return { ...state, templates: action.payload };
+    case SAVE_SELECTED_TEMPLATE:
+      return { ...state, selectedTemplate: { ...action.payload } };
+    case SAVE_RESUME_DETAIL:
+      return { ...state, resumeDetail: { ...action.payload } };
+    case SAVE_DIRECTION:
+      return { ...state, selectedDirection: { ...action.payload } };
+    case SAVE_SELECT_COURSE:
+      return { ...state, resumeCourse: { ...action.payload } };
+    case SAVE_EDIT_DATA:
+      return { ...state, editData: action.payload };
+    case SAVE_EDIT_STATUS:
+      return { ...state, resumeEditStatus: action.payload };
+    case SAVE_RESUME_REFERENCE:
+      return { ...state, resumeReference: action.payload };
+    default:
+      return state;
+  }
 }
--- src/store/reducers/user.js
+++ src/store/reducers/user.js
@@ -1,37 +1,37 @@
-import { SAVE_EDIT_USERINFO, SAVE_USER_INFO } from "../types";
+import { SAVE_EDIT_USERINFO, SAVE_USER_INFO } from '../types';
 
 const initState = {
-  	userInfo: {},
-	editUserInfo: {
-		name: "",
-		sex: "",
-		bornTime: "", 
-		degree: "", 
-		graduateTime: "",
-		college: "",
-		major: {}, 
-		phone: "", 
-		email: "",
-		avatar: {},
-		politics_status: "",
-		origin: ""
-	}
-}
+  userInfo: {},
+  editUserInfo: {
+    name: '',
+    sex: '',
+    bornTime: '',
+    degree: '',
+    graduateTime: '',
+    college: '',
+    major: {},
+    phone: '',
+    email: '',
+    avatar: {},
+    politics_status: '',
+    origin: ''
+  }
+};
 
 export default function user(state = initState, action) {
-	switch(action.type) {
-		case SAVE_EDIT_USERINFO:
-			if(action.payload.clear) {
-				return { ...state, editUserInfo: action.payload.data };
-			}else {
-				let type = action.payload.type;
-				let editUserInfo = state.editUserInfo;
-				editUserInfo[type] = action.payload.data;
-				return { ...state, editUserInfo: {...editUserInfo} };
-			}
-		case SAVE_USER_INFO:
-			return  {...state, userInfo: action.payload};
-		default:
-			return state;
-	}
+  switch (action.type) {
+    case SAVE_EDIT_USERINFO:
+      if (action.payload.clear) {
+        return { ...state, editUserInfo: action.payload.data };
+      } else {
+        let type = action.payload.type;
+        let editUserInfo = state.editUserInfo;
+        editUserInfo[type] = action.payload.data;
+        return { ...state, editUserInfo: { ...editUserInfo } };
+      }
+    case SAVE_USER_INFO:
+      return { ...state, userInfo: action.payload };
+    default:
+      return state;
+  }
 }
